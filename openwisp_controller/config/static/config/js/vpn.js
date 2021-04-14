012416d8d335af506c462788d44b9ec7a626ab89
Gagan Deep <pandafy.dev@gmail.com>
2021-04-14 07:56:02 +0530

    [requested-change] Fixed hiding/showing related fields on VPN admin

--- openwisp_controller/config/static/config/js/vpn.js
+++ openwisp_controller/config/static/config/js/vpn.js
@@ -1,54 +1,63 @@
 'use strict';
 django.jQuery(function ($) {
-    if (!$('.add-form').length) {
-        return;
+    if ($('.add-form').length) {
+        var showOverlay = function () {
+            var loading = $('#loading-overlay');
+            if (!loading.length) {
+                $('body').append(
+                    '<div id="loading-overlay" class="djnjc-overlay loading"><div class="spinner"></div></div>'
+                );
+                loading = $('#loading-overlay');
+            }
+            loading.fadeIn(100, function () {
+                loading.css('display', 'flex');
+                var spinner = loading.find('.spinner');
+                spinner.fadeOut(100, function () {
+                    var message = gettext(
+                        'Please be patient, we are creating all the necessary ' +
+                        'cyrptographic keys which may take some time'
+                    );
+                    spinner.remove();
+                    loading.append('<p>');
+                    loading.find('p').hide().text(message).fadeIn(250);
+                });
+            });
+        };
+
+        $('#vpn_form').submit(function () {
+            showOverlay();
+        });
     }
 
-    var showOverlay = function () {
-        var loading = $('#loading-overlay');
-        if (!loading.length) {
-            $('body').append(
-                '<div id="loading-overlay" class="djnjc-overlay loading"><div class="spinner"></div></div>'
-            );
-            loading = $('#loading-overlay');
-        }
-        loading.fadeIn(100, function () {
-            loading.css('display', 'flex');
-            var spinner = loading.find('.spinner');
-            spinner.fadeOut(100, function () {
-                var message = gettext(
-                    'Please be patient, we are creating all the necessary ' +
-                        'cyrptographic keys which may take some time'
-                );
-                spinner.remove();
-                loading.append('<p>');
-                loading.find('p').hide().text(message).fadeIn(250);
-            });
-        });
-    },
-    toggleIpSubnetFields = function () {
+    var toggleRelatedFields = function () {
         // Show IP and Subnet field only for Wireguard backend
-        if ($('#id_backend').val() == 'openwisp_controller.vpn_backends.Wireguard') {
-            $('label[for="id_subnet"]').parent().show();
-            $('label[for="id_ip"]').parent().show();
+        var backendValue = $('#id_backend').val() === undefined ? '' : $('#id_backend').val().toLocaleLowerCase().toLocaleLowerCase();
+        if (backendValue.includes('wireguard') || backendValue.includes('vxlan')) {
+            $('label[for="id_subnet"]').parent().parent().show();
+            $('label[for="id_ip"]').parent().parent().show();
+
         } else {
-            $('label[for="id_subnet"]').parent().hide();
-            $('label[for="id_ip"]').parent().hide();
+            $('label[for="id_subnet"]').parent().parent().hide();
+            $('label[for="id_ip"]').parent().parent().hide();
             // Reset IP and Subnet fields
             $('#id_subnet').val(null);
             $('#id_ip').val(null);
         }
-    };
 
-    $('#vpn_form').submit(function () {
-        showOverlay();
-    });
+        if (backendValue.includes('openvpn')) {
+            $('label[for="id_ca"]').parent().parent().show();
+            $('label[for="id_cert"]').parent().parent().show();
+        } else {
+            $('label[for="id_ca"]').parent().parent().hide();
+            $('label[for="id_cert"]').parent().parent().hide();
+        }
+    };
 
     // clean config when VPN backend is changed
     $('#id_backend').change(function () {
         $('#id_config').val('{}');
-        toggleIpSubnetFields();
+        toggleRelatedFields();
     });
 
-    toggleIpSubnetFields();
+    toggleRelatedFields();
 });
